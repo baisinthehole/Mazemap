a09913876dbe0a5b3e772e5b593b54753d86ef55
baisinthehole <helmer.n@hotmail.com>
2016-09-29 19:10:42 +0200

    added an array containing only hallways

--- Stuff.js
+++ Stuff.js
@@ -1,3 +1,6 @@
+// enum
+var RoomType = {"OFFICE": 1, "CORRIDOR": 2, "STAIRS": 3, "COMPUTER_LAB": 4, "MEETING_ROOM": 5, "LECTURE_HALL": 6, "STUDY_ROOM": 7, "NOT_AVAILABLE": 8, "TOILETS": 9, "STORAGE_ROOM": 10, "LAB": 11, "COPY_ROOM": 12, "TECHNICAL": 13, "WARDROBE": 14, "SHOWER": 15, "GROUP_ROOM": 16, "INSTITUTE": 17, "FRAT": 18, "DRAWING_ROOM": 19, "LIBRARY": 20, "TEACHING_ROOM": 21, "STORE": 22, "CANTEEN": 23, "SIT": 24, "BUS_STOP": 27, "PARKING_LOT": 28, "WORKSHOP": 29};
+
 // initialize rawResponse and event for asynchronous response from server
 // Create global variable for storing the geography JSON in
 var rawResponse;
@@ -9,9 +12,16 @@ var event = new Event('responseTextChange');
 var map = Maze.map('mazemap-container', { campusloader: false });
 map.setView([63.41431498967308,10.406826528933491], 15);
 
+// One array of coordinates for each type of polygon
+var roomCoordinates = [];
+var stairCoordinates = [];
+var outlineCoordinates = [];
+var doorCoordinates = [];
+var corridorCoordinates = [];
+
 // Uncomment the preferred JSON file
 getLocalJSON('floor_4_35.json');
-// getJSONfromServer();
+getJSONfromServer();
 
 
 /* JSON object from server */
@@ -25,33 +35,9 @@ function getJSONfromServer() {
 // This will contain the rest of the program, and will be run when we know we have received the JSON from the server
 function recievedJSONfromServer() {
     var geoJSON = JSON.parse(rawResponse);
-    var temp;
-    var color;
-    var coordinates = [];
-    console.log(geoJSON);
-    for (i = 0; i < geoJSON.pois.length; i++) {
-        color = 'blue';
-        coordinates.push(geoJSON.pois[i].geometry.coordinates[0]);
-        for (j = 0; j < coordinates[i].length; j++) {
-            temp = coordinates[i][j][0];
-            coordinates[i][j][0] = coordinates[i][j][1];
-            coordinates[i][j][1] = temp;
-        }
-        // Draws different rooms in different colors
-        for (var j = 0; j < geoJSON.pois[i].infos.length; j++) {
-            // WC
-            if (geoJSON.pois[i].infos[j].poiTypeId==9){
-            color = 'red';
-            }
-            // corridors
-            if (geoJSON.pois[i].infos[j].poiTypeId==2){
-            color = 'green';
-            }
-        }
-        Maze.polygon(coordinates[i], {color: color}).addTo(map);
-    }
+    var color = "red";
+    fillCoordinateTypeServer(geoJSON, corridorCoordinates, RoomType.CORRIDOR, color, "polygon");
 }
-
   // Function for requesting JSON object from server
 function getHttp(url) {
     var info = new XMLHttpRequest();
@@ -66,7 +52,7 @@ function getHttp(url) {
             document.getElementById('mazemap-container').dispatchEvent(event);
         }
         else {
-            console.error("Error", error);
+            //console.error("Error", error);
         }
     }
 }
@@ -81,26 +67,19 @@ function getLocalJSON(filename) {
 }
 function recievedLocalJSON(data) {
     var color = ['blue', 'gray', 'green', 'black'];
-    var temp;
-    var index = 0;
-
-    // One array of coordinates for each type of polygon
-    var roomCoordinates = [];
-    var stairCoordinates = [];
-    var outlineCoordinates = [];
-    var doorCoordinates = [];
     
     // Fill the coordinate arrays for each type of polygon and draw to map
-    fillCoordinateType(data, stairCoordinates, 'stairs', color[0]);
-    fillCoordinateType(data, roomCoordinates, 'rooms', color[1]);
-    fillCoordinateType(data, doorCoordinates, 'doors', color[2]);
-    fillCoordinateType(data, outlineCoordinates, 'outlines', color[3]);
+    fillCoordinateTypeLocal(data, stairCoordinates, 'stairs', color[0], "line");
+    fillCoordinateTypeLocal(data, roomCoordinates, 'rooms', color[1], "line");
+    fillCoordinateTypeLocal(data, doorCoordinates, 'doors', color[2], "line");
+    fillCoordinateTypeLocal(data, outlineCoordinates, 'outlines', color[3], "line");
+
 
     // Draw markers on all stair coordinates
-    drawMarkersForStairs(stairCoordinates);
+    //drawMarkersForStairs(stairCoordinates);
 }
 
-function fillCoordinateType(data, coordinates, coordinateType, color) {
+function fillCoordinateTypeLocal(data, coordinates, coordinateType, color, lineOrPolygon) {
     for (i = 0; i < data[coordinateType].features.length; i++) {
         coordinates.push(data[coordinateType].features[i].geometry.coordinates[0]);
         for (j = 0; j < coordinates[i].length; j++) {
@@ -108,7 +87,40 @@ function fillCoordinateType(data, coordinates, coordinateType, color) {
             coordinates[i][j][0] = coordinates[i][j][1];
             coordinates[i][j][1] = temp;
         }
-        Maze.polygon(coordinates[i], {color: color}).addTo(map);
+        if (lineOrPolygon == "line") {
+            Maze.polyline(coordinates[i], {color: color}).addTo(map);
+        }
+        else {
+            Maze.polygon(coordinates[i], {color: color}).addTo(map);
+        }
+    }
+    console.log(coordinates);
+}
+
+function fillCoordinateTypeServer(data, coordinates, coordinateType, color, lineOrPolygon) {
+    var temp;
+    for (i = 0; i < data.pois.length; i++) {
+        for (var j = 0; j < data.pois[i].infos.length; j++) {
+            if (data.pois[i].infos[j].poiTypeId == coordinateType){
+                coordinates.push(data.pois[i].geometry.coordinates[0]);
+
+                console.log(coordinates);
+
+                for (k = 0; k < coordinates[coordinates.length - 1].length; k++) {
+                    temp = coordinates[coordinates.length - 1][k][0];
+                    coordinates[coordinates.length - 1][k][0] = coordinates[coordinates.length - 1][k][1];
+                    coordinates[coordinates.length - 1][k][1] = temp;
+                }
+            }
+        }
+    }
+    for (i = 0; i < coordinates.length; i++) {
+        if (lineOrPolygon == "line") {
+            Maze.polyline(coordinates[i], {color: color}).addTo(map);
+        }
+        else {
+            Maze.polygon(coordinates[i], {color: color}).addTo(map);
+        }
     }
 }
 
