fed6642cc17170a3c474c2ad1d1b586bcb63e652
baisinthehole <helmer.n@hotmail.com>
2016-10-03 14:13:16 +0200

    works, but should remove hallways from regular roomcoordinates

--- Stuff.js
+++ Stuff.js
@@ -1,12 +1,19 @@
 // enum
 var RoomType = {"OFFICE": 1, "CORRIDOR": 2, "STAIRS": 3, "COMPUTER_LAB": 4, "MEETING_ROOM": 5, "LECTURE_HALL": 6, "STUDY_ROOM": 7, "NOT_AVAILABLE": 8, "TOILETS": 9, "STORAGE_ROOM": 10, "LAB": 11, "COPY_ROOM": 12, "TECHNICAL": 13, "WARDROBE": 14, "SHOWER": 15, "GROUP_ROOM": 16, "INSTITUTE": 17, "FRAT": 18, "DRAWING_ROOM": 19, "LIBRARY": 20, "TEACHING_ROOM": 21, "STORE": 22, "CANTEEN": 23, "SIT": 24, "BUS_STOP": 27, "PARKING_LOT": 28, "WORKSHOP": 29};
 
+
+var drawn = false;
+
+var threshold = 0.0005;
+
 // initialize rawResponse and event for asynchronous response from server
 // Create global variable for storing the geography JSON in
 var rawResponse;
 
 // Create event for handling asynchronous responses from the server
 var event = new Event('responseTextChange');
+
+var zoomLevelsDrawn = {"16": false, "17": false, "18": false, "19": false, "20": false};
 
 // Create a map
 var map = Maze.map('mazemap-container', { campusloader: false });
@@ -19,9 +26,73 @@ var outlineCoordinates = [];
 var doorCoordinates = [];
 var corridorCoordinates = [];
 
+var roomPolygons = [];
+var stairPolygons = [];
+var outlinePolygons = [];
+var doorPolygons = [];
+var corridorPolygons = [];
+
 // Uncomment the preferred JSON file
 getLocalJSON('floor_4_35.json');
 getJSONfromServer();
+
+// Zoom listener
+map.on('zoomend', function () {
+    console.log(map.getZoom());
+    if (!zoomLevelsDrawn["17"]) {
+        if (map.getZoom() >= 17) {
+            drawPolygons(outlinePolygons);
+            zoomLevelsDrawn["17"] = true;
+        }
+    }
+    if (zoomLevelsDrawn["17"]) {
+        if (map.getZoom() < 17) {
+            removePolygons(outlinePolygons);
+            zoomLevelsDrawn["17"] = false;
+        }
+    }
+    if (!zoomLevelsDrawn["18"]) {
+        if (map.getZoom() >= 18) {
+            console.log("why no corridors?");
+            drawPolygons(corridorPolygons);
+            zoomLevelsDrawn["18"] = true;
+        }
+    }
+    if (zoomLevelsDrawn["18"]) {
+        if (map.getZoom() < 18) {
+            removePolygons(corridorPolygons);
+            zoomLevelsDrawn["18"] = false;
+        }
+    }
+    if (!zoomLevelsDrawn["19"]) {
+        if (map.getZoom() >= 19) {
+            drawPolygonsBiggerThanThreshold(roomCoordinates, roomPolygons, threshold);
+            drawPolygons(stairPolygons);
+            zoomLevelsDrawn["19"] = true;
+        }
+    }
+    if (zoomLevelsDrawn["19"]) {
+        if (map.getZoom() < 19) {
+            removePolygonsBiggerThanThreshold(roomCoordinates, roomPolygons, threshold);
+            removePolygons(stairPolygons);
+            zoomLevelsDrawn["19"] = false;
+        }
+    }
+    if (!zoomLevelsDrawn["20"]) {
+        if (map.getZoom() >= 20) {
+            drawPolygonsSmallerThanThreshold(roomCoordinates, roomPolygons, threshold);
+            drawPolygons(doorPolygons);
+            zoomLevelsDrawn["20"] = true;
+        }
+    }
+    if (zoomLevelsDrawn["20"]) {
+        if (map.getZoom() < 20) {
+            removePolygonsSmallerThanThreshold(roomCoordinates, roomPolygons, threshold);
+            removePolygons(doorPolygons);
+            zoomLevelsDrawn["20"] = false;
+        }
+    }
+});
 
 
 /* JSON object from server */
@@ -36,7 +107,7 @@ function getJSONfromServer() {
 function recievedJSONfromServer() {
     var geoJSON = JSON.parse(rawResponse);
     var color = "red";
-    fillCoordinateTypeServer(geoJSON, corridorCoordinates, RoomType.CORRIDOR, color, "polygon");
+    fillCoordinateTypeServer(geoJSON, corridorCoordinates, corridorPolygons, RoomType.CORRIDOR, color, "polygon");
 }
   // Function for requesting JSON object from server
 function getHttp(url) {
@@ -69,17 +140,17 @@ function recievedLocalJSON(data) {
     var color = ['blue', 'gray', 'green', 'black'];
     
     // Fill the coordinate arrays for each type of polygon and draw to map
-    fillCoordinateTypeLocal(data, stairCoordinates, 'stairs', color[0], "line");
-    fillCoordinateTypeLocal(data, roomCoordinates, 'rooms', color[1], "line");
-    fillCoordinateTypeLocal(data, doorCoordinates, 'doors', color[2], "line");
-    fillCoordinateTypeLocal(data, outlineCoordinates, 'outlines', color[3], "line");
+    fillCoordinateTypeLocal(data, stairCoordinates, stairPolygons, 'stairs', color[0], "line");
+    fillCoordinateTypeLocal(data, roomCoordinates, roomPolygons, 'rooms', color[1], "line");
+    fillCoordinateTypeLocal(data, doorCoordinates, doorPolygons, 'doors', color[2], "line");
+    fillCoordinateTypeLocal(data, outlineCoordinates, outlinePolygons, 'outlines', color[3], "line");
 
 
     // Draw markers on all stair coordinates
     //drawMarkersForStairs(stairCoordinates);
 }
 
-function fillCoordinateTypeLocal(data, coordinates, coordinateType, color, lineOrPolygon) {
+function fillCoordinateTypeLocal(data, coordinates, polygonList, coordinateType, color, lineOrPolygon) {
     for (i = 0; i < data[coordinateType].features.length; i++) {
         coordinates.push(data[coordinateType].features[i].geometry.coordinates[0]);
         for (j = 0; j < coordinates[i].length; j++) {
@@ -88,23 +159,22 @@ function fillCoordinateTypeLocal(data, coordinates, coordinateType, color, lineO
             coordinates[i][j][1] = temp;
         }
         if (lineOrPolygon == "line") {
-            Maze.polyline(coordinates[i], {color: color}).addTo(map);
+            polygonList.push(Maze.polyline(coordinates[i], {color: color}));
         }
         else {
-            Maze.polygon(coordinates[i], {color: color}).addTo(map);
+            polygonList.push(Maze.polygon(coordinates[i], {color: color}));
         }
     }
     console.log(coordinates);
 }
 
-function fillCoordinateTypeServer(data, coordinates, coordinateType, color, lineOrPolygon) {
+function fillCoordinateTypeServer(data, coordinates, polygonList, coordinateType, color, lineOrPolygon) {
     var temp;
-    for (i = 0; i < data.pois.length; i++) {
+    for (var i = 0; i < data.pois.length; i++) {
         for (var j = 0; j < data.pois[i].infos.length; j++) {
             if (data.pois[i].infos[j].poiTypeId == coordinateType){
                 coordinates.push(data.pois[i].geometry.coordinates[0]);
 
-                console.log(coordinates);
 
                 for (k = 0; k < coordinates[coordinates.length - 1].length; k++) {
                     temp = coordinates[coordinates.length - 1][k][0];
@@ -114,12 +184,12 @@ function fillCoordinateTypeServer(data, coordinates, coordinateType, color, line
             }
         }
     }
-    for (i = 0; i < coordinates.length; i++) {
+    for (var i = 0; i < coordinates.length; i++) {
         if (lineOrPolygon == "line") {
-            Maze.polyline(coordinates[i], {color: color}).addTo(map);
+            polygonList.push(Maze.polyline(coordinates[i], {color: color}));
         }
         else {
-            Maze.polygon(coordinates[i], {color: color}).addTo(map);
+            polygonList.push(Maze.polygon(coordinates[i], {color: color}));
         }
     }
 }
@@ -143,10 +213,67 @@ function loadJSON(path, success, error)
     xhr.open("GET", path, true);
     xhr.send();
 }
+
 function drawMarkersForStairs(stairCoordinates) {
     for (var i = 0; i < stairCoordinates.length; i++) {
         for (var j = 0; j < stairCoordinates[i].length; j++) {
             L.marker(stairCoordinates[i][j]).addTo(map);
+        }
+    }
+}
+
+function getRoomCircumference(singleRoomCoordinates) {
+
+    var circumference = 0;
+
+    for (var i = 0; i < singleRoomCoordinates.length - 1; i++) {
+        circumference += Math.abs(singleRoomCoordinates[i + 1][0] - singleRoomCoordinates[i][0]);
+        circumference += Math.abs(singleRoomCoordinates[i + 1][1] - singleRoomCoordinates[i][1]);
+    }
+
+    return circumference;
+}
+
+function drawPolygons(polygonList) {
+    for (var i = 0; i < polygonList.length; i++) {
+        map.addLayer(polygonList[i]);
+    }
+}
+
+function removePolygons(polygonList) {
+    for (var i = 0; i < polygonList.length; i++) {
+        map.removeLayer(polygonList[i]);
+    }
+}
+
+function drawPolygonsBiggerThanThreshold(roomCoordinates, polygonList, threshold) {
+    for (var i = 0; i < polygonList.length; i++) {
+        if (getRoomCircumference(roomCoordinates[i]) > threshold) {
+            map.addLayer(polygonList[i]);
+        }
+    }
+}
+
+function removePolygonsBiggerThanThreshold(roomCoordinates, polygonList, threshold) {
+    for (var i = 0; i < polygonList.length; i++) {
+        if (getRoomCircumference(roomCoordinates[i]) > threshold) {
+            map.removeLayer(polygonList[i]);
+        }
+    }
+}
+
+function drawPolygonsSmallerThanThreshold(roomCoordinates, polygonList, threshold) {
+    for (var i = 0; i < polygonList.length; i++) {
+        if (getRoomCircumference(roomCoordinates[i]) < threshold) {
+            map.addLayer(polygonList[i]);
+        }
+    }
+}
+
+function removePolygonsSmallerThanThreshold(roomCoordinates, polygonList, threshold) {
+    for (var i = 0; i < polygonList.length; i++) {
+        if (getRoomCircumference(roomCoordinates[i]) < threshold) {
+            map.removeLayer(polygonList[i]);
         }
     }
 }
