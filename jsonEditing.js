81c50f16a1ae755877cdd59b1d3cfd269a9505ce
Morteano <morteano@stud.ntnu.no>
2017-03-16 12:48:42 +0100

    add corridors for floorId 300

--- jsonEditing.js
+++ jsonEditing.js
@@ -7,11 +7,17 @@ function alterJSONfile(JSON, ID) {
         }
     }
     if (ID == "76") {
-    	for (var i = 0; i < JSON.pois[213].infos.length; i++) {
+        for (var i = 0; i < JSON.pois[213].infos.length; i++) {
             if (JSON.pois[213].infos[i].poiTypeId == 238) {
                 JSON.pois[213].infos[i].poiTypeId = 2;
             }
         }
+    }
+    if (ID == "300") {
+        JSON.pois[22].infos.push({poiTypeId: 2});
+        JSON.pois[59].infos.push({poiTypeId: 2});
+        JSON.pois[137].infos.push({poiTypeId: 2});
+        JSON.pois[155].infos.push({poiTypeId: 2});
     }
     return JSON;
 }
