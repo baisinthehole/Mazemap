7a1f64dbd485df4b7d5b157ecd27daf70995d688
Morteano <morteano@stud.ntnu.no>
2017-04-24 10:33:53 +0200

    add a lot of icons

--- Stuff.js
+++ Stuff.js
@@ -9,6 +9,26 @@ Maze.Map = Maze.Map.extend({
         });
     }
 });
+Maze.Instancer.getPoisByCategoryAndCampusId(2, 1).then( function (pois) {
+    console.log(pois);
+});
+Maze.Data.getPoiCategoryIdsByCampusId(1).then( function (poistypes) {
+    console.log(poistypes);
+});
+Maze.Instancer.getCampus(1).then( function (campus) {
+    // MAP.fitBounds(campus.getBounds());
+    campus.addTo(MAP).setActive().then( function() {
+        MAP.setZLevel(1);
+        MAP.getZLevelControl().show();
+
+        campus.addPoiCategory(9);    // 9 = toilets
+        // campus.addPoiCategory(4);    // 4 = computer lab
+        // campus.addPoiCategory(7);    // 7 = study room
+        // campus.addPoiCategory(28);    // 28 = parking
+        // campus.addPoiCategory(27);    // 27 = bus stops
+    });
+});
+
 var globalMergedCorridorsCoordinates = [];
 // enum for all room types
 var ROOM_TYPE = {"OFFICE": 1, "CORRIDOR": 2, "STAIRS": 3, "COMPUTER_LAB": 4, "MEETING_ROOM": 5, "LECTURE_HALL": 6, "STUDY_ROOM": 7, "NOT_AVAILABLE": 8, "TOILETS": 9, "STORAGE_ROOM": 10, "LAB": 11, "COPY_ROOM": 12, "TECHNICAL": 13, "WARDROBE": 14, "SHOWER": 15, "GROUP_ROOM": 16, "INSTITUTE": 17, "FRAT": 18, "DRAWING_ROOM": 19, "LIBRARY": 20, "TEACHING_ROOM": 21, "STORE": 22, "CANTEEN": 23, "SIT": 24, "BUS_STOP": 27, "PARKING_LOT": 28, "WORKSHOP": 29, "ROOM":91};
